2ddd936ee3706625df65727de9a4beae645c7fb0
Yamada Masaki <masaki@hisme.net>
2011-10-03 08:37:30 +0900

    add version number to mona class

--- lib/mona.rb
+++ lib/mona.rb
@@ -1,4 +1,13 @@
 module Mona
+  autoload :Client, 'mona/client'
   autoload :Board, 'mona/board'
   autoload :Thread, 'mona/thread'
+
+  VERSION_MAJOR = 0
+  VERSION_MINOR = 1
+  VERSION_PATCH = 0
+
+  def self.version
+    [VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH].join('.')
+  end
 end
--- spec/mona_spec.rb
+++ spec/mona_spec.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Mona do
+  it { Mona.version.should eq '0.1.0' }
 end
